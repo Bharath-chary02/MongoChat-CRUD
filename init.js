55afde8500affdd1dccfd6d00bb7c0cf58bdb03a
Bharath-chary02 <kadiyalabharath98@gmail.com>
2026-01-26 11:39:00 +0530

    Add favicon for browser tab

--- init.js
+++ init.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const Chat = require("./models/chat");
+require("dotenv").config();
 
 main().then(()=>{
     console.log("Connection success")
 }).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/Whatsapp');
+  await mongoose.connect(process.env.MONGO_URL);
 }
 
 const allchats = [
